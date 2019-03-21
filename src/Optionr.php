cd0d83d24c6694a39958aec74927467682d07d1a
Christos Athanasiadis <christos.k.athanasiadis@gmail.com>
2019-03-21 16:55:11 +0200

    accepted values pool
    It's possible now to define at runtime a pool of values that the returned value should belong to. The validation applies to the default value as well.

--- src/Optionr.php
+++ src/Optionr.php
@@ -2,31 +2,25 @@
 declare(strict_types = 1);
 
 /*
- * Copyright (c) 2019 Christos Athanasiadis
+ * Copyright 2019 Christos Athanasiadis <christos.k.athanasiadis@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
- * copy
- * of this software and associated documentation files (the "Software"), to
- * deal
- * in the Software without restriction, including without limitation the
- * rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in
- * all
- * copies or substantial portions of the Software.
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE
- * SOFTWARE.
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  */
 
 /**
@@ -37,21 +31,30 @@ declare(strict_types = 1);
 namespace Kristos80\Optionr2;
 
 class Optionr implements \PetrKnap\Php\Singleton\SingletonInterface {
-	
+
 	use \PetrKnap\Php\Singleton\SingletonTrait;
-	
-	public function get($name = '', $pool = array(), $default = NULL, $sensitive = FALSE) {
-		if (! is_array($name)) {
+
+	/**
+	 *
+	 * @param string|array|object $name
+	 * @param array|object $pool
+	 * @param mixed $default
+	 * @param bool $sensitive
+	 * @param boolean|array $acceptedValues
+	 * @return mixed
+	 */
+	public function get($name = '', $pool = array(), $default = NULL, bool $sensitive = FALSE, $acceptedValues = FALSE) {
+		if (! is_array($name) && ! is_object($name)) {
 			if (! is_string($name) && ! is_numeric($name)) {
 				$name = (string) serialize($name);
 			}
 		}
-		
+
 		$pool = (array) $pool;
 		$option = $default;
-		
+
 		if (! $sensitive) {
-			if (! is_array($name)) {
+			if (! is_array($name) && ! is_object($name)) {
 				$name = ! is_numeric($name) ? strtolower($name) : $name;
 			} else {
 				$name_ = array();
@@ -60,15 +63,15 @@ class Optionr implements \PetrKnap\Php\Singleton\SingletonInterface {
 				}
 				$name = $name_;
 			}
-			
+
 			$pool_ = array();
 			foreach ($pool as $poolKey => $poolValue) {
 				$pool_[strtolower((string) $poolKey)] = $poolValue;
 			}
-			
+
 			$pool = $pool_;
 		}
-		
+
 		if (is_array($name)) {
 			foreach ($name as $possibleName) {
 				if (array_key_exists($possibleName, $pool)) {
@@ -81,7 +84,22 @@ class Optionr implements \PetrKnap\Php\Singleton\SingletonInterface {
 		} else {
 			$option = array_key_exists($name, $pool) ? ($pool[$name] ? $pool[$name] : $option) : $option;
 		}
-		
+
+		if ($acceptedValues !== FALSE) {
+			if (! is_array($acceptedValues)) {
+				$acceptedValues = FALSE;
+			}
+		}
+
+		if ($acceptedValues !== FALSE) {
+			if (! in_array($option, $acceptedValues)) {
+				if (! in_array($default, $acceptedValues)) {
+					$default = NULL;
+				}
+				$option = $default;
+			}
+		}
+
 		return $option;
 	}
 }
